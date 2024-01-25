ad1daca16051c1a832f38a383000b58f2009a463
srlnapr <serlinaprilia32@gmail.com>
2024-01-25 09:21:10 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Halo nama saya {{$name}}</h2>
+    <P>Saya beralamat di {{$address}}</P>
+</body>
+</html>
